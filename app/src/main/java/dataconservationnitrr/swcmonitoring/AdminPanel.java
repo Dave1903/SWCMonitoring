94df1f279af304b34ef752d8a2f11a5b4e725e41
ketankmr <ketank619@gmail.com>
2018-12-03 20:46:41 +0530

    admin panel

---
+++ app/src/main/java/dataconservationnitrr/swcmonitoring/AdminPanel.java
@@ -0,0 +1,24 @@
+package dataconservationnitrr.swcmonitoring;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+public class AdminPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(new AdminPanelAdapter(getSupportFragmentManager()));
+
+        // Give the PagerSlidingTabStrip the ViewPager
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/dataconservationnitrr/swcmonitoring/AdminPanelAdapter.java
@@ -0,0 +1,43 @@
+package dataconservationnitrr.swcmonitoring;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+
+/**
+ * Created by Ketan-PC on 12/18/2017.
+ */
+
+public class AdminPanelAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] { "Approved", "Pending"};
+
+
+    public AdminPanelAdapter(FragmentManager fm) {
+        super(fm);
+
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0) {
+            return new WaitingFragment();
+        }
+        else {
+            return new ApprovedFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
