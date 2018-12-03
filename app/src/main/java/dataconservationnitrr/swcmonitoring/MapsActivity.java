9fec02d334a7303eb3ec14f0eefeb12741f4a25a
Dave.lopers_Home <19davinder@gmail.com>
2018-12-03 17:52:33 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

--- app/src/main/java/dataconservationnitrr/swcmonitoring/MapsActivity.java
+++ app/src/main/java/dataconservationnitrr/swcmonitoring/MapsActivity.java
@@ -1,7 +1,21 @@
 package dataconservationnitrr.swcmonitoring;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,18 +24,133 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+    static final int Request_Camera  = 2 ;
+    private File imageFile;
     private GoogleMap mMap;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == Request_Camera && resultCode == RESULT_OK ) {
+            if(imageFile!= null) {
+                if(imageFile.exists()) {
+                 //   Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getPath());
+
+
+                }
+
+
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"File Not saved ",Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+
+
+
+    public void  openCamera(){
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE }, Request_Camera);
+        }
+        else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                imageFile = createImageFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (imageFile!=null) {
+                Uri tempUri = FileProvider.getUriForFile(MapsActivity.this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        imageFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+
+                startActivityForResult(intent, Request_Camera);
+            }else{
+                Toast.makeText(getApplicationContext(),"Error opening camera",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==Request_Camera){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
+
+
+    }
+
+    private File createImageFile()  {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "GIS");
+        File image = null;
+
+        if(!storageDir.exists()){
+
+            storageDir.mkdirs();
+
+        }
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+
+        return image;
     }
 
 
